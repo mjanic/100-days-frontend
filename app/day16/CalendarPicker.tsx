7dbfc55469d2b4519263cd1fd65419d5a5b52d50
mjanic <forpay777@gmail.com>
2024-09-05 18:50:50 +0200

    day17

--- app/day16/CalendarPicker.tsx
+++ app/day16/CalendarPicker.tsx
@@ -2,7 +2,7 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
-import { format } from "date-fns";
+import { addDays, format } from "date-fns";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -13,39 +13,61 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Label } from "@/components/ui/label";
+import { DateRange } from "react-day-picker";
 
 import { useState } from "react";
 
-interface CalendarPickerProps {
-  label: string;
-}
-
-export default function CalendarPicker({ label }: CalendarPickerProps) {
-  const [date, setDate] = useState<Date>();
+export default function CalendarPicker() {
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: addDays(new Date(), 5),
+  });
 
   return (
     <div className="flex flex-col gap-2">
-      <Label>{label}</Label>
+      <Label className="text-xs font-bold">Check In</Label>
       <Popover>
         <PopoverTrigger asChild>
           <Button
             variant={"outline"}
             className={cn(
-              "w-[240px] justify-between text-left font-normal rounded-sm",
+              "w-full h-9 text-xs justify-between text-left font-normal rounded-sm",
               !date && "text-muted-foreground"
             )}
           >
-            {date ? format(date, "PPP") : <span>Pick a date</span>}
+            {date?.from ? (
+              format(date.from, "LLL dd, y")
+            ) : (
+              <span>Pick a date</span>
+            )}
             <FontAwesomeIcon icon={faCalendarDays} width={15} height={15} />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
+        <PopoverContent className="w-auto p-0" align="start" side="right">
           <Calendar
-            mode="single"
+            mode="range"
             selected={date}
             onSelect={setDate}
             initialFocus
           />
+        </PopoverContent>
+      </Popover>
+      <Label className="text-xs font-bold">Check Out</Label>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-full h-9 text-xs justify-between text-left font-normal rounded-sm",
+              !date && "text-muted-foreground"
+            )}
+          >
+            {date?.to ? format(date.to, "LLL dd, y") : <span>Pick a date</span>}
+            <FontAwesomeIcon icon={faCalendarDays} width={15} height={15} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start" side="right">
+          <Calendar mode="range" selected={date} onSelect={setDate} />
         </PopoverContent>
       </Popover>
     </div>
--- app/day16/page.tsx
+++ app/day16/page.tsx
@@ -6,13 +6,71 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 import CalendarPicker from "./CalendarPicker";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function Day14() {
   return (
-    <div className="h-[100vh] p-[10%] flex items-center justify-center">
-      <div className="bg-white w-full h-full p-4 rounded-lg shadow-2xl flex">
-        <div className="w-[40%] h-full flex flex-col gap-2">
-          <CalendarPicker label="Check In" />
-          <CalendarPicker label="Check Out" />
+    <div className="h-[100vh] p-20 flex items-center justify-center">
+      <div className="bg-white w-full max-w-[800px] h-full p-4 rounded-lg shadow-2xl flex">
+        <div className="w-[40%] h-full flex flex-col gap-2 p-1 pr-4">
+          <CalendarPicker />
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <Label className="text-xs font-bold">Adults</Label>
+              <Select>
+                <SelectTrigger className="text-xs h-9">
+                  <SelectValue placeholder="1" />
+                </SelectTrigger>
+                <SelectContent className="text-xs">
+                  <SelectItem className="text-xs" value="1">
+                    1
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="2">
+                    2
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="3">
+                    3
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="4">
+                    4
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label className="text-xs font-bold">Children</Label>
+              <Select>
+                <SelectTrigger className="text-xs h-9">
+                  <SelectValue placeholder="1" />
+                </SelectTrigger>
+                <SelectContent className="text-xs">
+                  <SelectItem className="text-xs" value="1">
+                    1
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="2">
+                    2
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="3">
+                    3
+                  </SelectItem>
+                  <SelectItem className="text-xs" value="4">
+                    4
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <Button className="bg-orange-400 mt-auto h-9 text-xs">
+            Book a room
+          </Button>
         </div>
         <div className="h-full w-[60%]">
           <div className="flex flex-col h-full">
